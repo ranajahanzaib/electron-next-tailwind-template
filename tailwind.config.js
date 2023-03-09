0e27ab9e0c3615a6f8cc6519bd9d0a0ecda8fd6c
Aftab Rehan <workwithaftabrehan@gmail.com>
2023-03-10 00:24:53 +0500

    Configure Tailwind CSS (#1)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./renderer/pages/*.{js,ts,jsx,tsx}",
+    "./renderer/pages/**/*.{js,ts,jsx,tsx}",
+    "./renderer/src/components/*.{js,ts,jsx,tsx}",
+    "./renderer/src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// const d = require("./renderer/src/components/");
