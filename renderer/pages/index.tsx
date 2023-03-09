eafe80371bbea822ce159f5b5623a1f5d87728d9
aftabrehan <aftabrehan52422@gmail.com>
2023-03-10 00:18:12 +0500

    Configure Tailwind CSS

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -1,32 +1,32 @@
-import { useEffect } from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { useEffect } from "react";
+import Link from "next/link";
+import Layout from "../src/components/Layout";
 
 const IndexPage = () => {
   useEffect(() => {
-    const handleMessage = (_event, args) => alert(args)
+    const handleMessage = (_event, args) => alert(args);
 
     // add a listener to 'message' channel
-    global.ipcRenderer.addListener('message', handleMessage)
+    global.ipcRenderer.addListener("message", handleMessage);
 
     return () => {
-      global.ipcRenderer.removeListener('message', handleMessage)
-    }
-  }, [])
+      global.ipcRenderer.removeListener("message", handleMessage);
+    };
+  }, []);
 
   const onSayHiClick = () => {
-    global.ipcRenderer.send('message', 'hi from next')
-  }
+    global.ipcRenderer.send("message", "hi from next");
+  };
 
   return (
     <Layout title="Home | Next.js + TypeScript + Electron Example">
-      <h1>Hello Next.js 👋</h1>
+      <h1 className="flex text-red-500">Hello Next.js 👋</h1>
       <button onClick={onSayHiClick}>Say hi to electron</button>
       <p>
         <Link href="/about">About</Link>
       </p>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
